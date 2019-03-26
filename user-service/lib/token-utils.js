2d5301a9d618f919ad6abe074178fc1928793bb7
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-03-26 14:49:29 +0200

    Finish basic functionality of user-service

--- user-service/lib/token-utils.js
+++ user-service/lib/token-utils.js
@@ -1,6 +1,7 @@
+const config = require('config');
 const jwt = require('jsonwebtoken');
 
-const SECRET = 'testsecret'; // FIXME: Improve secret handling
+const SECRET = config.get('user-service.auth-token.secret');
 
 function genAuthToken(id) {
     return jwt.sign({ id }, SECRET, { expiresIn: '1y' });
--- user-service/routes/index.js
+++ user-service/routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const HTTP = require('http-status');
+
+const tokenUtils = require('../lib/token-utils');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
+router.get('/health', (req, res) => {
+    res.status(HTTP.OK).end();
+});
+
+router.get('/api/auth/verify', (req, res) => {
+    const authToken = req.get('Authorization').replace('Bearer ', '');
+
+    try {
+        const { id } = tokenUtils.verifyAuthToken(authToken);
+        res.json({ id });
+    } catch (err) {
+        res.status(HTTP.UNAUTHORIZED).end();
+    }
 });
 
 module.exports = router;
