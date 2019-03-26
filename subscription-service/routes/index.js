c088540170271e7a4b0304ff5a9cd12aba2553b3
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-03-26 16:00:58 +0200

    Add a mocked endpoint for adding a subscription

--- subscription-service/routes/index.js
+++ subscription-service/routes/index.js
@@ -1,10 +1,22 @@
+const _ = require('lodash');
 const express = require('express');
 const HTTP = require('http-status');
+
+const userValidation = require('../middleware/user-validation');
 
 const router = express.Router();
 
 router.get('/health', (req, res) => {
     res.status(HTTP.OK).end();
+});
+
+router.post('/api/subscriptions', [userValidation], (req, res) => {
+    if (!_.isEmpty(req.user)) {
+        res.status(HTTP.CREATED).end();
+        return;
+    }
+
+    res.status(HTTP.INTERNAL_SERVER_ERROR).end();
 });
 
 module.exports = router;
