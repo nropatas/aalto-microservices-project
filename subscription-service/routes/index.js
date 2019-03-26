6d8d3d4364595f3622464c9fd593534ac750067f
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-03-26 15:31:05 +0200

    Add health check for subscription service

--- subscription-service/routes/index.js
+++ subscription-service/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const HTTP = require('http-status');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
+router.get('/health', (req, res) => {
+    res.status(HTTP.OK).end();
 });
 
 module.exports = router;
