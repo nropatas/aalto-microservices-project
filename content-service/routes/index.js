6433bcc2d5283ff7bfa6ef51749d4d8cc6324afb
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-04-04 21:51:52 +0300

    Add route for getting subscribed contents

--- content-service/routes/index.js
+++ content-service/routes/index.js
@@ -1,10 +1,22 @@
+const common = require('common');
+const config = require('config');
 const express = require('express');
 const HTTP = require('http-status');
+
+const { createUserValidation } = common.middleware;
 
 const router = express.Router();
 
 router.get('/health', (req, res) => {
     res.status(HTTP.OK).end();
+});
+
+router.get('/api/contents', [
+    createUserValidation({ userServiceUrl: config.get('services.user-service.url') }),
+], (req, res) => {
+    // TODO: Get list of subscriptions from subscription-service
+
+    // TODO: Get contents from APIs
 });
 
 module.exports = router;
